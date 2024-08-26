7f7db10ce9effd7b6ad3432427a3932668d3eb28
floweeb <floydqaranja@gmail.com>
2024-08-26 22:42:42 +0300

    The player scene is moving in all direction withoutanimations though

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,67 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-var current_dir = "none"
-
-func _ready() -> void:
-	$AnimatedSprite2D.play("front_idle")
+var speed: float = 100
+var accel: float = 10
 
 func _physics_process(_delta: float) -> void:
-	player_movement(_delta)
+	var dir = Input.get_vector("left", "right", "up", "down")
 	
-func player_movement(_delta: float) -> void:
+	velocity.x = move_toward(velocity.x, speed * dir.x, accel)
+	velocity.y = move_toward(velocity.y, speed * dir.y, accel)
 	
-	if Input.is_action_pressed("ui_right"):
-		current_dir = "right"
-		play_animation(1)
-		velocity.x = SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		current_dir = "left"
-		play_animation(1)
-		velocity.x = -SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "down"
-		play_animation(1)
-		velocity.x = 0
-		velocity.y = SPEED
-	elif Input.is_action_pressed("ui_up"):
-		current_dir = "up"
-		play_animation(1)
-		velocity.x = 0
-		velocity.y = -SPEED
-	else: 
-		play_animation(0)
-		velocity.x = 0
-		velocity.y = 0
 	move_and_slide()
-	
-func play_animation(movement: int) -> void:
-	var dir = current_dir
-	var anim = $AnimatedSprite2D
-	
-	if dir == "right":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("side_walk")
-		elif movement == 0:
-			anim.play("side_idle")
-	elif dir == "left":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("side_walk")
-		elif movement == 0:
-			anim.play("side_idle")
-	elif dir == "down":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("front_walk")
-		elif movement == 0:
-			anim.play("front_idle")
-	elif dir == "up":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("back_walk")
-		elif movement == 0:
-			anim.play("back_idle")
